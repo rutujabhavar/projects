7afcef2a9fff659fbb5985b32abb6363a3c15a5e
RUTUJA BHAVAR <rutujabhavar95@gmail.com>
2025-11-25 14:04:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This ensures Tailwind scans all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
